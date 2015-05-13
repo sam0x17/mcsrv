73356e15ce3d4d0f169d42f5d8349dc7f2bb7f4f
Sam Kelly <sam@applicationsonline.com>
2015-05-13 17:21:13 -0400

    Use define_accessor! and begin switching to forked process.

--- lib/mcsrv/minecraft_server.rb
+++ lib/mcsrv/minecraft_server.rb
@@ -1,18 +1,28 @@
-
 require 'mcsrv/version'
-
+require 'mcsrv/syntactic_sugar'
 module MCSRV
   class MinecraftServer
-    @@server_ids = {}
+    extend MCSRV::SyntacticSugar
+    define_accessor! :default_hostname, 'localhost'
+    define_accessor! :default_port, 25565
+    define_accessor! :default_ram, "1024M"
+    define_accessor! :server_ids, {}
 
     def new(options={})
+      set_default = Proc.new {|key,value| options[key] = value if !options.has_key?(key) }
       allowed_options = [:id, :version, :hostname, :port, :ram, :world_path]
+      set_default.(:id, "server#{server_ids.size}")
+      set_default.(:version, Version::latest)
+      set_default.(:port, Server::default_port)
+      set_default.(:ram, Server::default_ram)
     end
 
     def start
       Dir.chdir 'versions'
       puts Dir.pwd
-      exec('java -Xmx1024M -Xms1024M -jar minecraft_server.1.8.3.jar nogui')
+      pin, ppout = IO.pipe
+      ppin, pout = IO.pipe
+      #exec('java -Xmx1024M -Xms1024M -jar minecraft_server.1.8.3.jar nogui')
     end
 
   end
